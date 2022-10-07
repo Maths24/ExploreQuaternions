6e87b9626c58699d39b282cf3600dff3931db68c
Maths24 <matthi24.m@web.de>
2022-10-07 18:31:32 +0200

    Initial Commit

---
+++ WSeminar/WSeminarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WSeminarApp.swift
+//  WSeminar
+//
+//  Created by Matthias on 07.10.22.
+//
+
+import SwiftUI
+
+@main
+struct WSeminarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
